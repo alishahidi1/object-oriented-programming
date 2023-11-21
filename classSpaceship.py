79a1fd0377ed84b6046c4462b37009e70c3dd12c
alishahidi1 <ali.shahidi@uwaterloo.ca>
2023-11-21 02:32:11 -0500

    overload instantiation

--- classSpaceship.py
+++ classSpaceship.py
@@ -1,17 +1,23 @@
 class Spaceship():
-    def __init__(self):
+    
+    def __init__(self,name=None):
+        if name is None:
+            self.callSign = "Nameless spaceship"
+            self._shieldStrength = 100
+        else:
+            self.callSign = name
+            self._shieldStrength = 100
 
-        #instace variables
-        self.callSign = ''
-        self._shieldStrength = 100
 
-        #methods
-        def fireMissile(self):
-            return "Pew!"
-        
-        def reduceShield(self, amount):
-            self._shieldStrength -= amount
+
+    #methods
+    def fireMissile(self):
+        return "Pew!"
+    
+    def reduceShield(self, amount):
+        self._shieldStrength -= amount
         
 
 #instantiation
 myShip = Spaceship()
+print(myShip.callSign)
