7a59f50d69427db121c1010f2a391990b6a717e0
alishahidi1 <ali.shahidi@uwaterloo.ca>
2023-11-21 01:57:09 -0500

    calss added

---
+++ classSpaceship.py
@@ -0,0 +1,14 @@
+class Spaceship():
+    def __init__(self):
+
+        #instace variables
+        self.callSign = ''
+        self._shieldStrength = 100
+
+        #methods
+        def fireMissile(self):
+            return "Pew!"
+        
+        def reduceShield(self, amount):
+            self._shieldStrength -= amount
+        
